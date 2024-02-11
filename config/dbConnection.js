b3d17c06f3e21a884ddc8f82e62812258f76fb7c
M-Abdullah-Bhatti <muhammadabdullahimdad@gmail.com>
2024-02-11 18:41:56 +0500

    small change

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const connectDatabase = () => {
   mongoose.connect(process.env.MONGO_URI).then((data) => {
-    // console.log(`Monogodb connected with server : ${data.connection.host} `)
     console.log(`Monogodb connected with server`);
   });
 };
