989395f13c76d156fbd07c06b1677ade334af4bf
M-Abdullah-Bhatti <87266652+M-Abdullah-Bhatti@users.noreply.github.com>
2024-02-21 01:18:08 +0500

    small change

--- server.js
+++ server.js
@@ -5,16 +5,6 @@ const cors = require("cors");
 const { default: mongoose } = require("mongoose");
 require("dotenv").config();
 
-// app.use(
-//   cors({
-//     origin: [
-//       "http://localhost:3000",
-//       //   "https://fan-gram-admin-panel.vercel.app",
-//       //   "https://fan-gram.vercel.app",
-//     ],
-//   })
-// );
-
 app.use(
   cors({
     origin: "*",
